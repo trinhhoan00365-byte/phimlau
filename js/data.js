dbb840496dc4ba68b8f363e5c5348811ffec67f9
trinhhoan00365-byte <trinhhoan00365@gmail.com>
2025-12-16 01:28:14 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,23 @@
-const videos = Array.from({ length: 25 }).map((_, i) => ({
-  id: i + 1,
-  title: `Video ${i + 1}`,
-  thumb: `https://picsum.photos/400/225?random=${i + 1}`,
-  duration: `${Math.floor(Math.random() * 10) + 1}:${Math.floor(Math.random() * 60).toString().padStart(2, "0")}`,
-  embed: "https://www.youtube.com/embed/dQw4w9WgXcQ"
-}));
+const videos = [
+  {
+    id: 1,
+    title: "Tên video 1",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_20251215aa9012462dc8191cab8681ca09ceb437.jpg",
+    duration: "6:21",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 2,
+    title: "Tên video 2",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "12:05",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 3,
+    title: "Tên video 3",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_20251215ff9056cc6924c970929635b6c3d47cf8.jpg",
+    duration: "4:48",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  }
+];
