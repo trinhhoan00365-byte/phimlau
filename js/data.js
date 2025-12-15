95d092ba47ad2cf3e1a020bb34b408da1f843727
trinhhoan00365-byte <trinhhoan00365@gmail.com>
2025-12-16 01:33:38 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,23 +1,30 @@
 const videos = [
   {
     id: 1,
-    title: "Tên video 1",
-    thumb: "https://sf-static.upanhlaylink.com/img/image_20251215aa9012462dc8191cab8681ca09ceb437.jpg",
+    title: "Video 1",
+    thumb: "https://i.imgur.com/8Km9tLL.jpg",
     duration: "6:21",
-    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+    embed: "https://www.youtube.com/embed/dQw4w9WgXcQ"
   },
   {
     id: 2,
-    title: "Tên video 2",
-    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    title: "Video 2",
+    thumb: "https://i.imgur.com/4AiXzf8.jpg",
     duration: "12:05",
-    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+    embed: "https://www.youtube.com/embed/9bZkp7q19f0"
   },
   {
     id: 3,
-    title: "Tên video 3",
-    thumb: "https://sf-static.upanhlaylink.com/img/image_20251215ff9056cc6924c970929635b6c3d47cf8.jpg",
+    title: "Video 3",
+    thumb: "https://i.imgur.com/Z6X6Z4P.jpg",
     duration: "4:48",
-    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+    embed: "https://www.youtube.com/embed/3tmd-ClpJxA"
+  },
+  {
+    id: 4,
+    title: "Video 4",
+    thumb: "https://i.imgur.com/Qr71crq.jpg",
+    duration: "9:12",
+    embed: "https://www.youtube.com/embed/L_jWHffIx5E"
   }
 ];
