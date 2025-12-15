84ea89e43ca21bb62fe5d9643b69ea5300c768cf
trinhhoan00365-byte <trinhhoan00365@gmail.com>
2025-12-16 01:42:36 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,29 +2,148 @@ const videos = [
   {
     id: 1,
     title: "Video 1",
-    thumb: "https://i.imgur.com/8Km9tLL.jpg",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_20251215ff9056cc6924c970929635b6c3d47cf8.jpg",
     duration: "6:21",
-    embed: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
   },
   {
     id: 2,
     title: "Video 2",
-    thumb: "https://i.imgur.com/4AiXzf8.jpg",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_20251215aa9012462dc8191cab8681ca09ceb437.jpg",
     duration: "12:05",
-    embed: "https://www.youtube.com/embed/9bZkp7q19f0"
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
   },
   {
     id: 3,
     title: "Video 3",
-    thumb: "https://i.imgur.com/Z6X6Z4P.jpg",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
     duration: "4:48",
-    embed: "https://www.youtube.com/embed/3tmd-ClpJxA"
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
   },
   {
     id: 4,
     title: "Video 4",
-    thumb: "https://i.imgur.com/Qr71crq.jpg",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
     duration: "9:12",
-    embed: "https://www.youtube.com/embed/L_jWHffIx5E"
-  }
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 5,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 6,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 7,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 8,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 9,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 10,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 11,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 12,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 13,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 14,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 15,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 16,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 17,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 18,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 19,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 20,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
+  {
+    id: 21,
+    title: "Video 4",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512155b1705144ae210acb9d44eb55bc52afb.jpg",
+    duration: "9:12",
+    embed: "https://iframe.mediadelivery.net/embed/564734/23999165-d615-430f-b741-06c5c611c9b6"
+  },
 ];
