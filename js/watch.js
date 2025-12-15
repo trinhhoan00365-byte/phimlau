81643b84fc8e3928ccb40209ebb2779c78282388
trinhhoan00365-byte <trinhhoan00365@gmail.com>
2025-12-16 01:03:21 +0700

    Update watch.js

--- js/watch.js
+++ js/watch.js
@@ -1,10 +1,11 @@
 const params = new URLSearchParams(location.search);
 const id = Number(params.get("id"));
 
-const video = videos.find(v => v.id === id);
-
 const iframe = document.getElementById("player");
 const overlay = document.getElementById("playerOverlay");
+const related = document.getElementById("related");
+
+const video = videos.find(v => v.id === id);
 
 if (video) {
   document.getElementById("title").textContent = video.title;
@@ -14,3 +15,15 @@ if (video) {
     overlay.style.display = "none";
   };
 }
+
+// VIDEO KHÁC
+videos.filter(v => v.id !== id).slice(0, 6).forEach(v => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <img class="thumb" src="${v.thumb}">
+    <h3>${v.title}</h3>
+  `;
+  card.onclick = () => location.href = `watch.html?id=${v.id}`;
+  related.appendChild(card);
+});
