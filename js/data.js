ac4294a9be80018a7cbab8f54d67e40f29edf188
trinhhoan00365-byte <trinhhoan00365@gmail.com>
2025-12-16 01:17:21 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,5 +2,6 @@ const videos = Array.from({ length: 25 }).map((_, i) => ({
   id: i + 1,
   title: `Video ${i + 1}`,
   thumb: `https://picsum.photos/400/225?random=${i + 1}`,
+  duration: `${Math.floor(Math.random() * 10) + 1}:${Math.floor(Math.random() * 60).toString().padStart(2, "0")}`,
   embed: "https://www.youtube.com/embed/dQw4w9WgXcQ"
 }));
